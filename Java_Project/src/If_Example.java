3004736b68fa39ba6df7878ebbddd75b5d5921fa
pc <pc@pc-PC>
2019-06-04 21:20:51 +0530

    Testing If, If else and Else If statements.

---
+++ Java_Project/src/If_Example.java
@@ -0,0 +1,48 @@
+
+public class If_Example {
+
+	public static void main(String[] args) {
+		
+		int a = 40;
+		
+		// If Statement
+		
+		if (a > 30) {
+			
+			System.out.println("If statement is True");
+			
+		}
+		
+		// If Else Statement
+		
+		if (a > 50) {
+			
+			System.out.println("Satement is True");
+			
+		} else {
+			
+			System.out.println("Else Statement is True");
+
+		}
+		
+		// Else If Statement 
+		
+		if (a > 45) {
+			
+			System.out.println("Statement is True");
+			
+		} else if(a > 55 ) {
+			
+			System.out.println("Else Statement is True");
+			
+			}
+		else {
+			
+			System.out.println("Else if Statement is True");
+			
+		}
+		
+
+	}
+
+}
